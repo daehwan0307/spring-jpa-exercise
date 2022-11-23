2d0a3a1ae91392edbc56b02e56e8178dfd85cda0
daehwan0307 <eoghks960307@naver.com>
2022-11-23 14:14:32 +0900

    Add Repository

---
+++ src/main/java/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import entity.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author,Long> {
+}
