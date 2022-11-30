bdb1e2dfd80069de0980e24c121aa0e510146950
daehwan0307 <eoghks960307@naver.com>
2022-11-30 16:11:18 +0900

    수정

---
+++ src/main/java/domain/Teacher.java
@@ -0,0 +1,14 @@
+package domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class Teacher {
+    private String name;
+    private boolean isLikeAlgorithm;
+    private boolean isLikeSpringBoot;
+}
